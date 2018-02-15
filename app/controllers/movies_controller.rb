f032572cdec805a6e3429cd13dc6342f1ffcd246
afzalmasra125 <afzalmasra@gmail.com>
2018-02-14 21:30:44 -0600

    first day

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,11 @@
 class MoviesController < ApplicationController
 
   def index
-    movies = Movie.all
+    @movies = Movie.all
     render json: movies.as_json
     end
   def create
-    movies = Movie.new( {
+    @movies = Movie.new( {
                         name: params[:name],
                         summary: params[:summary],
                         actor: params[:actor],
@@ -20,5 +20,16 @@ class MoviesController < ApplicationController
     movies = Movie.find(params[:id])
     render json: movies.as_json
   end
+  def update
+    @movie = Movie.find(params[:id])
+    
+    @movie.name = params[:name] || @movie.name
+    @movie.summary = params[:summary] || @movie.summary
+    @movie.actor = params[:actor] || @movie.actor
+    @movie.rating = params[:rating] || @movie.rating
+    @movie.genre = params[:genre] || @movie.genre
+    @movie.content_url = params[:content_url] || @movie.content_url
+    movies.save
+    render json: movies.as_json
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
    get '/movies' => 'movies#index'
    post '/movies' => 'movies#create'
    get '/movies/:id' => 'movies#show'
+   patch "movies/:id" => 'movies#update'
+
 end
