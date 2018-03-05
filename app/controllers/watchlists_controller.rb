15a05112a9d8a54504effb7e3edf95830568530a
afzalmasra125 <afzalmasra@gmail.com>
2018-03-04 21:29:38 -0600

    Home page images align properly

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,12 +1,14 @@
 class WatchlistsController < ApplicationController
+    def index
+    watchlist = Watchlist.all
+    render json: watchlist.as_json
+  end
   def create
     watchlist = Watchlist.new({
                       user_id: current_user.id,
                       movie_id: params[:movie_id]  
                       })
     watchlist.save
-    p "******"
-    p watchlist.errors.full_messages
     render json: watchlist.as_json
   end
 end 
